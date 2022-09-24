47ee1f27282deafe8088e87b78fbfde6c21e053a
JarminthonEnriqueRuedaLarrotaADSO <pocopoc4158j@gmail.com>
2022-09-24 15:29:18 -0500

    Ejercicio 3

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 addEventListener("DOMContentLoaded", ()=>{
-    // 2.Calcule el mayor de tres números, permitiendo leer 3 valores diferentes
+//3. Desarrollar un diagrama que lea 3 valores diferentes e indique cual es el mayor de ellos, el menor o si son iguales
     let numero1 = Number(prompt('Ingrese un numero'))
     let numero2 = Number(prompt('Ingrese un numero'))
     let numero3 = Number(prompt('ingreese un numero'))
@@ -9,7 +9,15 @@ addEventListener("DOMContentLoaded", ()=>{
     else if ( numero2 > numero1 && numero2 > numero3) {
         console.log(`El 2do numero "${numero2}" es el mayor `);
     }
-    else {
+    else if(numero3 > numero1 && numero3 > numero2) {
         console.log(`El  3er numero "${numero3}" es el mayor`);
     }
+    // si son Menores
+    if(numero1<numero2 && numero1<numero3){console.log(`El  1er numero "${numero1}" es el menor`);}
+    else if(numero2<numero1 && numero2< numero3){console.log(`El  2do numero "${numero2}" es el menor`);}
+    else{console.log(`El  3er numero "${numero3}" es el menor`);}
+    // si son iguales
+    if(numero1 == numero2){console.log(`El  1er numero ${numero1} y el segundo "${numero2}" son iguales `);}
+    else if(numero3 == numero1){console.log(`El  3er numero "${numero3}" es igual al primer numero "${numero1}"`);}
+    else if(numero3 == numero2){console.log(`El  3er numero "${numero3}" y el segundo "${numero2}" son iguales`);}
 })
