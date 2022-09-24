bab17700434506df9b12e662442469fba5ba15de
JarminthonEnriqueRuedaLarrotaADSO <pocopoc4158j@gmail.com>
2022-09-24 16:58:08 -0500

    Ejercicio 4

--- main.js
+++ main.js
@@ -1,23 +1,21 @@
 addEventListener("DOMContentLoaded", ()=>{
-//3. Desarrollar un diagrama que lea 3 valores diferentes e indique cual es el mayor de ellos, el menor o si son iguales
-    let numero1 = Number(prompt('Ingrese un numero'))
-    let numero2 = Number(prompt('Ingrese un numero'))
-    let numero3 = Number(prompt('ingreese un numero'))
-    if (numero1 > numero2 && numero1> numero3) {
-        console.log(`El 1er numero "${numero1}" es el mayor`);
-    } 
-    else if ( numero2 > numero1 && numero2 > numero3) {
-        console.log(`El 2do numero "${numero2}" es el mayor `);
+    // 4. Cálculo de áreas - Elige una figura geométrica:" Triángulo y Círculo
+    // ¿Qué figura quiere calcular (Escriba T o C)?
+    // Triangulo = base * altura / 2
+    // Circulo = PI * radio* radio
+    
+    let calculoArea = prompt('ingrese t para triangulo o c para circulo')
+    // Triangulo
+    if(calculoArea == 't'){
+        let base = Number(prompt('ingrese el balor de la base '))
+        let altura = Number(prompt('ingrese el valor de la altura'))
+        let areaTriangulo = (base*altura)/2
+        console.log(`El area del Triangulo es ${areaTriangulo}`)
     }
-    else if(numero3 > numero1 && numero3 > numero2) {
-        console.log(`El  3er numero "${numero3}" es el mayor`);
+    // Circulo
+    else if (calculoArea == 'c'){
+        let radio = Number(prompt('ingrese el valor del radio'))
+        let areaCirculo = (Math.PI*radio*radio)
+        console.log(`El area del circulo es ${areaCirculo}`)
     }
-    // si son Menores
-    if(numero1<numero2 && numero1<numero3){console.log(`El  1er numero "${numero1}" es el menor`);}
-    else if(numero2<numero1 && numero2< numero3){console.log(`El  2do numero "${numero2}" es el menor`);}
-    else{console.log(`El  3er numero "${numero3}" es el menor`);}
-    // si son iguales
-    if(numero1 == numero2){console.log(`El  1er numero ${numero1} y el segundo "${numero2}" son iguales `);}
-    else if(numero3 == numero1){console.log(`El  3er numero "${numero3}" es igual al primer numero "${numero1}"`);}
-    else if(numero3 == numero2){console.log(`El  3er numero "${numero3}" y el segundo "${numero2}" son iguales`);}
 })
