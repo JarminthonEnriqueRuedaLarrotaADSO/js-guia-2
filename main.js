250792b779951bcd6354dcb2c26ec3f421a4cdf1
JarminthonEnriqueRuedaLarrotaADSO <pocopoc4158j@gmail.com>
2022-09-24 19:26:41 -0500

    Ejercicio 13

--- main.js
+++ main.js
@@ -1,16 +1,32 @@
 addEventListener("DOMContentLoaded", ()=>{
-    // 12.Restarle al primer número al segundo (siempre y cuando el primero sea mayor que él segundo, en
-    // caso contrario indicar con un mensaje que la operación no es posible realizarla.
+    // 13. En una tienda de HELADO da un descuento por compra a sus clientes con membresía dependiendo
+    // de su tipo, sólo existen tres tipos de membresía, tipo A, tipo B y tipo C.
+    // Los descuentos son los siguientes:
+    // Tipo A 10% de descuento
+    // Tipo B 15% de descuento
+    // Tipo C 20% de descuento
     var i
+    console.log(`tipos de membresía, tipo A, tipo B y tipo C.`)
     for(i=1;i<=1;i+=1) {
-        let numero1 = Number(prompt('Ingrese el primer numero'))
-        let numero2 = Number(prompt('Ingrese el segundo numero'))
-        if (numero1 > numero2){
-            let solucion = numero1-numero2
-            console.log(`El resultado de la resta es: "${solucion}"`)
+        let compra = Number(prompt('ingrese el costo de su compra'))
+        let membresia = (prompt('ingrese el tipo de membresia a la que pertenece '))
+    // MEMBRESIA TIPO A
+        if (membresia == 'a'){
+            let descuento =compra*0.10
+            let aplicacionDescuento =compra-descuento
+            console.log(`El valor a pagar es: ${aplicacionDescuento}`)
         }
-        else{
-            console.log(`no es posible realizar la operacion...`)
+    // MEMBRESIA TIPO B
+        else if (membresia == 'b'){
+            let descuento =compra*0.15
+            let aplicacionDescuento =compra-descuento
+            console.log(`El valor a pagar es: ${aplicacionDescuento}`)
         }
-    }
+    // MEMBRESIA TIPO C
+        else if (membresia == 'c'){
+            let descuento = compra*0.20
+            let aplicacionDescuento =compra-descuento
+            console.log(`El valor a pagar es: ${aplicacionDescuento}`)
+        }
+}
 })
